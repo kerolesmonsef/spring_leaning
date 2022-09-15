559a8211f6a1c980c78e98578a7f4e84bc6ec612
kero <keroles.monsef@tecbadia.com>
2022-09-15 17:06:52 +0200

    begin wiht pagination

--- src/main/java/com/example/qgame/helpers/filters/products/IFilterOption.java
+++ src/main/java/com/example/qgame/helpers/filters/products/IFilterOption.java
@@ -1,9 +1,25 @@
 package com.example.qgame.helpers.filters.products;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Predicate;
+import java.util.Map;
 
 public abstract class IFilterOption {
+
+    private CriteriaBuilder cb;
+
     public abstract String getName();
 
     protected abstract Predicate getPredict();
+
+    public final Predicate execute(Predicate lastPredicate) {
+        if (lastPredicate == null) {
+            return getPredict();
+        }
+
+        return cb.and(
+                lastPredicate,
+                getPredict()
+        );
+    }
 }
--- src/main/java/com/example/qgame/helpers/filters/products/filterOptionsImpls/CategoryFilterOption.java
+++ src/main/java/com/example/qgame/helpers/filters/products/filterOptionsImpls/CategoryFilterOption.java
@@ -1,11 +1,22 @@
 package com.example.qgame.helpers.filters.products.filterOptionsImpls;
 
+import com.example.qgame.Models.Category;
+import com.example.qgame.QGameApplication;
 import com.example.qgame.helpers.filters.IFilter;
 import com.example.qgame.helpers.filters.products.IFilterOption;
 
 import javax.persistence.criteria.Predicate;
+import java.util.List;
+import java.util.Map;
 
 public class CategoryFilterOption extends IFilterOption {
+
+    private List<Long> ids;
+
+
+    public CategoryFilterOption(List<Long> ids) {
+        this.ids = ids;
+    }
 
     @Override
     public String getName() {
