c398613780c63ff433cfde79546f959d61a55ae7
kero <keroles.monsef@tecbadia.com>
2022-09-19 12:48:41 +0200

    df

--- src/main/java/com/example/qgame/helpers/filters/products/FilterOptionFilter.java
+++ src/main/java/com/example/qgame/helpers/filters/products/FilterOptionFilter.java
@@ -37,6 +37,7 @@ public class FilterOptionFilter {
 //                getCategoriesId()
                 getOptionValues()
         );
+
     }
 
     //--------------------------------------------------------
